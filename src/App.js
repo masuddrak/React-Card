85b3bec50aba49f8580fc563419c45a1f39580b6
masud rana <masudranaacb@gmail.com>
2022-03-16 15:58:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+const students=[
+  {name:' Sakibal Hassan',division:'Mathematics',id:52456},
+  {name:' RJ Kibria',division:'Chemistry',id:52458},
+  {name:' Litondas',division:'Economics',id:52459},
+  {name:' Polas Hosain',division:'Bangla',id:52457},
+]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {   
+       students.map(student=><Student name={student.name} division={student.division} id={student.id}></Student>)
+     }
+    </div>
+  );
+}
+function Student(props) {
+  return (
+    <div className="student">
+      <h3>Name:{props.name}</h3>
+      <p>Division:{props.division}</p>
+      <p>ID:{props.id}</p>
     </div>
   );
 }
